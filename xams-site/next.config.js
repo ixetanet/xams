1e403568d907fc6617e2dd1b587a53c78ba2dd06
Ben <benlabbe2007@gmail.com>
2024-06-15 08:03:01 -0500

    Build main site as static
    Build main site as static

--- xams-site/next.config.js
+++ xams-site/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
